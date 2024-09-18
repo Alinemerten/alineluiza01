78d62fc7b37778ecef3c64332cb8e38f1fb2ac60
Alinemerten <161774304+Alinemerten@users.noreply.github.com>
2024-09-18 19:42:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,58 @@
+criaCartao(
+    'Matemática',
+    'Quala raiz de 16?',
+    'A raiz quadrada de 16 é 4.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um ângulo reto?',
+    'Um ângulo reto mede 90 graus.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Casmurro"?',
+    '"Dom Casmurro" foi escrito por Machado de Assis.'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual é o tema principal de "A Moreninha"?',
+    'O tema principal é o amor jovem e as relações sociais do século XIX.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é a fórmula da água?',
+    'A fórmula da água é H₂O.'
+)
+criaCartao(
+    'Ciências',
+    'O que é fotossíntese?',
+    'Fotossíntese é o processo pelo qual as plantas convertem luz solar em energia.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'O primeiro presidente dos Estados Unidos foi George Washington.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano terminou a Segunda Guerra Mundial?',
+    'A Segunda Guerra Mundial terminou em 1945.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'O maior país do mundo é a Rússia.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O rio mais longo do mundo é o Nilo.'
+)
